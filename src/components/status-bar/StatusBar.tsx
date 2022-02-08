726f997c75e07660dc6c81d7560dd8551ca2eca8
christopher.fernandes <christyabcd.05@gmail.com>
2022-02-08 18:31:16 +0530

    Task #0000:  Fixed status alignment

--- src/components/status-bar/StatusBar.tsx
+++ src/components/status-bar/StatusBar.tsx
@@ -14,10 +14,10 @@ export const StatusBar = ({ label, status }: StatusBarProps) => {
     return (
         <div className={`${styles.status_bar}`}>
             {status && (status === "New" || status === 'Submitted') && (
-                <label className={`${styles.status_one_label} px-3`}>{`Status: ${label}`}</label>
+                <label className={`${styles.status_one_label} px-3 m-0`}>{`Status: ${label}`}</label>
             )}
             {status && status === "Under inspection" && (
-                <label className={`${styles.status_two_label} px-3`}>{`Status: ${label}`}</label>
+                <label className={`${styles.status_two_label} px-3 m-0`}>{`Status: ${label}`}</label>
             )}
         </div>
     )
