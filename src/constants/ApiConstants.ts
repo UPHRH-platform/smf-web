b3b791e630a12d59da021aa988f21437999d8647
Tushar <30565750+tushar5526@users.noreply.github.com>
2022-10-03 18:08:41 +0530

    Hard code SMF_BASE_URL

--- src/constants/ApiConstants.ts
+++ src/constants/ApiConstants.ts
@@ -1,5 +1,5 @@
 export const APIS = {
-  BASE_URL: process.env.REACT_APP_SMF_BASE_URL || "https://smfdev.idc.tarento.com/api/",
+  BASE_URL: process.env.REACT_APP_SMF_BASE_URL || "http://157.230.176.228/api/",
   LOGIN: {
     USERLOGIN: "signIn",
     REQUEST_OTP: "user/requestOTP",
